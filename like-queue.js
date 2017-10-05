c49881719fb4d63b373281c978592bb61e7f8185
Annie Bougie <anne.bougie@gmail.com>
2017-10-05 10:16:40 -0500

    remove console logs

--- like-queue.js
+++ like-queue.js
@@ -2,13 +2,11 @@ var Queue = require('firebase-queue');
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./browncountylibraryevents-firebase-adminsdk-ar0mr-71ef294f02.json");
-console.log('hi');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://browncountylibraryevents.firebaseio.com"
 });
 
-console.log('initialized');
 var db = admin.database();
 var instructorsRef = db.ref('instructors');
 
@@ -19,16 +17,14 @@ function processLikes() {
 
     console.log('start queue');
     var queue = new Queue(queueRef, function(data, progress, resolve, reject)  {
-        console.log('getting item');
-         progress(10);
-
-        console.log('queue processing ', data.instructorKey);
-        progress(90);
+        progress(10);
         var instructorRef = instructorsRef.child(data.instructorKey);
         instructorRef.once('value', function(snapshot) {
+            progress(90);
             var instructor = snapshot.val();
             var likes = instructor.likes + 1;
             instructorRef.update({likes: likes})
+                .then(() => console.log(`like processed for ${data.instructorKey}`))
                 .then(resolve)
                 .catch(reject);
         });
